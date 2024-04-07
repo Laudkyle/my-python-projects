042459bff9945b72967efaa36a9492370ef47e70
Laudkyle <kyleaby1@gmail.com>
2024-04-07 07:52:25 +0000

    first test

--- Python Scripts/text to speech/text.py
+++ Python Scripts/text to speech/text.py
@@ -6,7 +6,7 @@ engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
 engine.setProperty('rate', 150)
-# FIrst
+# FIrst Test
 engine.say('What is your name?')
 r = sr.Recognizer()
 with sr.Microphone() as source:
